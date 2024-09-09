e2fd1856f4c51afb25a2c3042f8f9d644e36aeb4
turjojoadder <turjojoadder10@gmail.com>
2024-09-09 22:21:27 +0600

    approval waiting

---
+++ database/migrations/2024_09_09_161125_alter_approval_column_in_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // Change the column type and default value
+            $table->boolean('approval')->default(true)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // Revert the column back to string and set it to nullable
+            $table->string('approval')->nullable()->change();
+        });
+    }
+};
